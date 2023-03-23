4619f7460d33513286da7a7a26698f5be16b3be2
Juan Salazar <ferthelet@gmail.com>
2023-03-23 09:57:42 -0500

    adds scuba distance flow control

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  // Write your code here! using if
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  // Write your code here! using ternary
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  // Write your code here! using switch
 }

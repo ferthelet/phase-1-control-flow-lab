e776df1275b18816f7c24882226c7c38f5f3f00e
Juan Salazar <ferthelet@gmail.com>
2023-03-23 10:12:15 -0500

    adds tip thank you feedback

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ function ternaryCheckCity(city){
   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here! using switch
+  switch(tip) {
+    case 'generous' :
+      return 'Thank you so much.';
+    case 'not as generous' :
+      return 'Thank you.';
+    default : return 'Bye.';
+  }
 }
